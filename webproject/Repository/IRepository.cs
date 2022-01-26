182aaa150a60daccec1d538894dc0354a2ef44ee
tugaspkl <highwaynormal2@gmail.com>
2022-01-26 23:46:10 +0700

    tugas

---
+++ webproject/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using webproject.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace webproject.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Add(T item);
+        void Remove(int id);
+        void Update(T item);
+        T FindByID(int id);
+        IEnumerable<T> FindAll();
+    }
+}
